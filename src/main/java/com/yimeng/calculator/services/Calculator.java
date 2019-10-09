324831fc0efa119c47b0a47e07ebfb08d6881962
Yimeng Li <yimengli@YimengdeMacBook-Pro.local>
2019-10-08 23:46:27 -0500

    Add sum logic, controller and unit test

---
+++ src/main/java/com/yimeng/calculator/services/Calculator.java
@@ -0,0 +1,10 @@
+package com.yimeng.calculator.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
