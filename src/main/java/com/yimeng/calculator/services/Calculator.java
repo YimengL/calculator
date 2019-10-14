cdd99ee4355698824c8a7f7ba5ca4603fedfd8b9
Yimeng Li <yimengli@Yimengs-MacBook-Pro.local>
2019-10-13 20:09:44 -0500

    add static analysis

--- src/main/java/com/yimeng/calculator/services/Calculator.java
+++ src/main/java/com/yimeng/calculator/services/Calculator.java
@@ -2,6 +2,9 @@ package com.yimeng.calculator.services;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class: {@link Calculator}.
+ */
 @Service
 public class Calculator {
     public int sum(int a, int b) {
